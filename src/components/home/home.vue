86347f718f20e143432c3ac16df02ae0b3803b18
heshij <1634943446@qq.com>
2020-04-17 18:27:59 +0800

    init project

---
+++ src/components/home/home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">这是主页</div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<style scoped lang="stylus">
+.home
+  color #ff0e2e
+</style>
